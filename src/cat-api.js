cc72c0744c609ac3723ff41a160bc958c699494a
robertSovar <133514926+robertSovar@users.noreply.github.com>
2023-11-19 23:36:54 +0200

    almost done

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,20 +1,26 @@
 import axios from "axios";
+import Notiflix from "notiflix";
 
 axios.defaults.headers.common["x-api-key"] = "live_9JseXOGGCFgzQKnj0Euj906Xb2AGlNH6wFwrtYc8oiiFbKMYXhC1t791Kgd2G5jc";
 
 export function fetchBreeds() {
     const catList =  axios.get('https://api.thecatapi.com/v1/breeds')
     .then(response => response.data)
-    .catch(error => { throw error});
-    return catList
+    .catch(error => { 
+        error = Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!')
+    });
+    return catList;
 };
 
 
-// export function fetchCatByBreed(breedId) {
-//     const showData = axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-//     .then(response => response.data[0])
-//     .catch(error => { throw error});
-//     console.log(showData);
-//     return showData;
-// }
+export function fetchCatByBreed(breedId) {
+    const showData = axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(response => response.data)
+    .catch(error => { 
+        error = Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!')
+    });
+    console.log(showData);
+    return showData;
+}
 
--- src/index.js
+++ src/index.js
@@ -1,38 +1,73 @@
-import { fetchBreeds } from "./cat-api.js";
-import { fetchCatByBreed } from "./cat-api.js";
+import { fetchBreeds, fetchCatByBreed } from "./cat-api.js";
+import SlimSelect from 'slim-select';
 
- 
+
+const slimSelect = new SlimSelect({
+//   select: '.breed-select'
+});
+
+
+const selectBreedEl = document.querySelector('.breed-select');
+const loaderEL = document.querySelector('.loader');
+const errorEl = document.querySelector('.error');
+const catInfoEl = document.querySelector('.cat-info');
+
 document.addEventListener('DOMContentLoaded', () => {
 
-    const selectBreedEl = document.querySelector('.breed-select');
-    const loaderEL = document.querySelector('.loader');
-    const errorEl = document.querySelector('.error');
-    const catInfoEl = document.querySelector('.cat-info');
-
-    loaderEL.style.display = 'none';
+    selectBreedEl.style.display = 'none';
+    loaderEL.style.display = 'block';
+    loaderEL.style.fontSize = '25px';
+    loaderEL.style.fontWeight = '700';
     errorEl.style.display = 'none';
     catInfoEl.style.display = 'none';
 
     try {
 
-        selectBreedEl.addEventListener('click',() => {
-            const breeds = fetchBreeds();
-            console.log(breeds);
-            breeds.then (res => {
-                res.map(breed => {
-                    const option = document.createElement("option");
-                    option.value = breed.id;
-                    option.textContent = breed.name;
-                    selectBreedEl.appendChild(option)
-                }); 
-            })
-            
-        });
-        
+        setTimeout(() => {
+            loaderEL.style.display = 'none';
+            selectBreedEl.style.display = 'block';
+        }, 1000);
+
+        setTimeout(getCatList, 0);
+        selectBreedEl.addEventListener('change', (e) => {
+                const target = e.target.value;
+                const catTarget = fetchCatByBreed(target);
+                loaderEL.style.display = 'block';
+                catTarget.then(res => {
+                   res.find(cat => {
+                     const markup =   `
+                <img src="${cat.url}" alt="Image of ${cat.breeds[0].name}"  height = "300">
+               <div class="container">
+                 <h2 class="title">${cat.breeds[0].name} </h2>
+                 <p class="description"> ${cat.breeds[0].description}</p>
+                 <h3 class="temperament">Temperament:</h3> <p>${cat.breeds[0].temperament}</p>
+               </div>`;
+               catInfoEl.style.display = 'flex';
+               catInfoEl.style.gap = '10px';
+               catInfoEl.style.paddingTop = '15px';
+               catInfoEl.innerHTML = markup;
+               loaderEL.style.display = 'none';
+                    });
+                });
+            });
     }
 
     catch (error) {
-     errorEl.style.display = 'none';
+     errorEl.style.display = 'block';
     }
 });
+
+function getCatList() {
+    const breeds = fetchBreeds();
+    breeds.then (res => {
+        res.map(breed => {
+            const option = document.createElement("option");
+            option.value = breed.id;
+            option.textContent = breed.name;
+            selectBreedEl.appendChild(option);
+        }); 
+    });
+};
+
+
 
